912c9a32905bb8f75206b91606d91d325de4f443
Jona <jbrown6@berklee.edu>
2021-05-11 23:51:02 -0700

    Added table object with daily totals

--- scripts/app.js
+++ scripts/app.js
@@ -1,51 +1,48 @@
 'use strict';
 
-function randomCookies(max, min, avg) { // global function for randomizing the cookies.
-  let randomNum = (avg * (Math.random() * (max - min)) + min); // the absolute minimum should be min at the very end.
-  return randomNum;
-}
-
-function Place(name, minCustomers, maxCustomers, avgCookies) {
+function Place(name, minCustomers, maxCustomers, avgCookies, hourlyCookies, hours, total) {
   this.name = name;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCookies = avgCookies;
-  this.hourlyCookies = null;
-  this.hours = [];
+  this.hourlyCookies = hourlyCookies;
+  this.hours = hours;
+  this.total = total;
+
+}
+
+function randomCookies(max, min, avg) { // global function for randomizing the cookies.
+  let randomNum = Math.abs(Math.floor(avg * (Math.random() * (max - min)) + min)); // the absolute minimum should be min at the very end.
+  return randomNum;
 }
 
 Place.prototype.setHourlyCookies = function() { // creating a function that Place object can use.
-  this.hourlyCookies = (randomCookies(this.minCustomers, this.minCustomers, this.avgCookies));
+  this.hourlyCookies = (randomCookies(this.minCustomers, this.maxCustomers, this.avgCookies));
 }
 
-let Seattle = new Place('Seattle', 23, 65, 6.3);
-let Tokyo = new Place('Tokyo', 3, 24, 1.2);
-let Dubai = new Place('Dubai', 11, 38, 3.7);
-let Paris = new Place('Tokyo', 20, 38, 2.3);
-let Lima = new Place('Tokyo', 2, 16, 4.6);
+Place.prototype.generateCookies = function() { //creating a function that Place object can use.
+    for (let a = 0; a < 14; a++) {
+      this.setHourlyCookies();
+      this.hours[a] = this.hourlyCookies
+    }
+    let total = 0;
+    for (let b = 0; b < 14; b++) {
+    total = total + this.hours[b];
+    }
+    this.total = total;
+}
+
+let Seattle = new Place('Seattle', 23, 65, 6.3, 0, [], 0);
+let Tokyo = new Place('Tokyo', 3, 24, 1.2, 0, [], 0);  
+let Dubai = new Place('Dubai', 11, 38, 3.7, 0, [], 0);
+let Paris = new Place('Paris', 20, 38, 2.3, 0, [], 0);
+let Lima = new Place('Lima', 2, 16, 4.6, 0, [], 0);
 
 console.log(Seattle.name);
 
-Place.prototype.setHourlyCookies = function() { //creating a function that Place object can use.
-    let total = 0;
-    let cookies = 0;
-    for (let a = 7; a < 13; a++) {
-      this.setHourlyCookies();
-      cookies = this.hourlyCookies;
-      this.hours.push(`${a}am: ${cookies} cookies`);
-      total += cookies;
-    }
-    for(let b = 1; b < 9; b++) {
-      this.setHourlyCookies();
-      cookies = this.hourlyCookies;
-      this.hours.push(`${b}pm: ${cookies} cookies`);
-      total += cookies;
-    }
-  this.hours.push(`Total: ${total} cookies`);
-}
 
 function createHeader() {
-  let times = ['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM','7:00PM'];
+  let times = ['6:00AM', '7:00AM', '8:00AM', '9:00AM', '10:00AM', '11:00AM', '12:00PM', '1:00PM', '2:00PM', '3:00PM', '4:00PM', '5:00PM', '6:00PM','7:00PM', 'Daily Location Total'];
 
   const header = document.getElementById('table-head'); // Assigns a variable to the header element in HTML.
 
@@ -56,28 +53,69 @@ function createHeader() {
   table.appendChild(tHeadElem);
 
   let trElem = document.createElement('tr');
-  
+  tHeadElem.appendChild(trElem);
 
-  
-
+  let buffer = document.createElement('th')
+  trElem.appendChild(buffer);
+  let thElem;
   for (let a = 0; a < times.length; a++) {
-
+    thElem = document.createElement('th');
+    thElem.textContent = times[a];
+    trElem.appendChild(thElem);
   }
 }
+
 function createTable(Place) {
 
+  const header = document.getElementById('table-head');
 
-  const article = document.createElement('article');
-  table.appendChild(article);
+  const tbodyElem = document.createElement('tbody');
+  header.appendChild(tbodyElem);
 
-  const h1Elem = document.createElement('h1');
-  h1Elem.textContent = Place.name;
-  article.appendChild(h1Elem);
-
-  const tableElem = document.createElement('table');
-  article.appendChild(tableElem);
   const trElem = document.createElement('tr');
-  tableElem.appendChild(trElem);
-  
+  tbodyElem.appendChild(trElem);
 
+  const thElem = document.createElement('h2');
+  thElem.textContent = Place.name;
+  trElem.appendChild(thElem);
+
+
+  Place.generateCookies();
+
+  let tdElem = null;
+  for (let a = 0; a < 14; a++) {
+    tdElem = document.createElement('td');
+    tdElem.textContent = Place.hours[a];
+    trElem.appendChild(tdElem);
+  }
+
+  const totalElem = document.createElement('td');
+  totalElem.textContent = Place.total;
+  trElem.appendChild(totalElem);
 }
+
+function createFooter() {
+  const header = document.getElementById('table-head');
+
+  const tfootElem = document.createElement('tfoot');
+  header.appendChild(tfootElem);
+
+  const trElem = document.createElement('tr');
+  tfootElem.appendChild(trElem);
+
+  const buffer = document.createElement('td');
+  trElem.appendChild(buffer);
+
+  const tdElem = document.createElement('td');
+  for (let a = 0; a < 14; a++) {
+    tdElem.textContent = '1000';
+    trElem.appendChild(tdElem);
+  }
+}
+createHeader();
+createTable(Seattle);
+createTable(Tokyo);
+createTable(Dubai);
+createTable(Paris);
+createTable(Lima);
+createFooter();
